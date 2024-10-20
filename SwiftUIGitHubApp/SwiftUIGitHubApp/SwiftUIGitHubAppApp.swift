89446c216d043acef395743e78021f83b1595ae4
Viktor-Drykin <142505425+Viktor-Drykin@users.noreply.github.com>
2024-10-20 18:13:31 +0100

    init project

---
+++ SwiftUIGitHubApp/SwiftUIGitHubApp/SwiftUIGitHubAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIGitHubAppApp.swift
+//  SwiftUIGitHubApp
+//
+//  Created by Viktor Drykin on 20.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIGitHubAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
