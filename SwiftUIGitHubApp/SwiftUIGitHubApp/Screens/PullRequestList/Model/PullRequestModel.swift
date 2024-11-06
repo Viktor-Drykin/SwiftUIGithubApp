b9b634b9461dfc92e66d34c915ac1a7056530199
Viktor-Drykin <142505425+Viktor-Drykin@users.noreply.github.com>
2024-11-06 14:54:05 +0000

    created a skeleton for Pull Request screen

---
+++ SwiftUIGitHubApp/SwiftUIGitHubApp/Screens/PullRequestList/Model/PullRequestModel.swift
@@ -0,0 +1,8 @@
+//
+//  PullRequestModel.swift
+//  SwiftUIGitHubApp
+//
+//  Created by DataArt Viktor Drykin on 06.11.2024.
+//
+
+import Foundation
---
+++ SwiftUIGitHubApp/SwiftUIGitHubApp/Screens/PullRequestList/ViewModel/PullRequestListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  PullRequestListViewModel.swift
+//  SwiftUIGitHubApp
+//
+//  Created by Viktor Drykin on 06.11.2024.
+//
+
+import Foundation
