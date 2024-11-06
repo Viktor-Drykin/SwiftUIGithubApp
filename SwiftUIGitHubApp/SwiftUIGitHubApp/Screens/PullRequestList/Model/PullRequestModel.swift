bec0aadce454702285ec4c013f5c879659b9dedf
Viktor-Drykin <142505425+Viktor-Drykin@users.noreply.github.com>
2024-11-06 15:45:04 +0000

    add view model to a pull request screen

--- SwiftUIGitHubApp/SwiftUIGitHubApp/Screens/PullRequestList/Model/PullRequestModel.swift
+++ SwiftUIGitHubApp/SwiftUIGitHubApp/Screens/PullRequestList/Model/PullRequestModel.swift
@@ -2,7 +2,22 @@
 //  PullRequestModel.swift
 //  SwiftUIGitHubApp
 //
-//  Created by DataArt Viktor Drykin on 06.11.2024.
+//  Created by Viktor Drykin on 06.11.2024.
 //
 
 import Foundation
+
+struct PullRequestModel {
+    let title: String
+    let number: String
+    let name: String
+    let createdAt: String
+    let closedAt: String
+    let state: String?
+}
+
+extension PullRequestModel {
+    init(with prDTO: PullRequestDTO) {
+        self.init(title: prDTO., number: <#T##String#>, name: <#T##String#>, createdAt: <#T##String#>, closedAt: <#T##String#>, state: <#T##String?#>)
+    }
+}
--- SwiftUIGitHubApp/SwiftUIGitHubApp/Screens/PullRequestList/ViewModel/PullRequestListViewModel.swift
+++ SwiftUIGitHubApp/SwiftUIGitHubApp/Screens/PullRequestList/ViewModel/PullRequestListViewModel.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+import Combine
+
+class PullRequestListViewModel: ObservableObject {
+    
+    @Published var pullRequests: [PullRequestModel] = []
+
+    private let apiService: PullRequestService
+    private let repoName: String
+    private let userName: String
+    private var cancellables = Set<AnyCancellable>()
+
+    init(apiService: PullRequestService, repoName: String, userName: String) {
+        self.apiService = apiService
+        self.repoName = repoName
+        self.userName = userName
+    }
+
+    func fetchPullRequests() {
+        apiService.fetchPullRequests(with: userName, repo: repoName)
+            .map { returnedRepos in
+                <#code#>
+            }
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let error):
+                    // handle error
+                    break
+                }
+            } receiveValue: { [weak self] repos in
+                self?.pullRequests = repos
+            }
+            .store(in: &cancellables)
+    }
+}
